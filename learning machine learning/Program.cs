9bb94e000f3036781374c00918b8bdbee0c00e05
Wyperian <138237408+yousifj129@users.noreply.github.com>
2024-01-17 14:44:09 +0300

    some plotting testing

--- learning machine learning/Program.cs
+++ learning machine learning/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using learning_machine_learning.Simple;
+using ScottPlot;
+using ScottPlot.WinForms;
 
 /*double[] X = { 1, 2, 3, 4, 5 , 6, 7, 8, 9, 10, 11,12 };
 
@@ -19,7 +21,7 @@ for(int i = 1;i < X.Length; i++)
 }
 */
 
-NeuralNetworkSimple nn = new NeuralNetworkSimple(2,6,2 , NeuralNetworkSimple.ActivationMode.relu);
+/*NeuralNetworkSimple nn = new NeuralNetworkSimple(2,6,2 , NeuralNetworkSimple.ActivationMode.relu);
 double[] inp = new double[] { 0, 1 };
 double[] target = new double[] { 3, 2 };
 
@@ -32,4 +34,41 @@ double[] a = nn.feedforward(new double[] { 0, 1 });
 for(int i = 0;i < a.Length; i++)
 {
     Console.WriteLine(a[i]);
+}*/
+// Create some example data
+
+double xt(double t)
+{
+    return Math.Sin( t);
 }
+double yt(double t)
+{
+    return t*t;
+}
+
+int leng = 1000;
+double[] x = new double[leng];
+double[] y = new double[leng];
+
+for (int i = 0; i < leng; i++)
+{
+    x[i] = xt(((double)i) /50);
+    y[i] = yt(((double)i) *1000);
+}
+// Create a new ScottPlot figure
+ScottPlot.Plot plt = new();
+
+plt.Style.ColorGrids(new Color(100, 100, 100));
+plt.Style.ColorAxes(new Color(255,255,255));
+plt.Style.Background(new Color(10, 10, 10), new Color(0, 0, 0));
+// Plot the data as a line plot
+plt.Add.Scatter(x, y);
+
+// Customize the plot
+plt.Title("This Time, We Meet on a Perfect Life");
+plt.XLabel("X");
+plt.YLabel("Y");
+
+// Save the plot to a file
+plt.SavePng("line_plot.png",2000,2000);
+
