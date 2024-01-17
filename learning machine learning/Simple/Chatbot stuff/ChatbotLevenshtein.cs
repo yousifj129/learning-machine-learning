b0c1bacbc2b2672a0b0ca1ff039b833230cc5184
Wyperian <138237408+yousifj129@users.noreply.github.com>
2024-01-17 15:18:17 +0300

    simple chatbot implementation
    simple chatbot implementation

---
+++ learning machine learning/Simple/Chatbot stuff/ChatbotLevenshtein.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learning_machine_learning.Simple.Chatbot_stuff
+{
+    internal class ChatbotLevenshtein : SimpleChatbot
+    {
+        public string[] questions;
+        public string[] answers;
+
+        public ChatbotLevenshtein(string[] questions1, string[] answers1)
+        {
+            questions = questions1;
+            answers = answers1;
+        }
+        public ChatbotLevenshtein()
+        {
+            questions = new string[0];
+            answers = new string[0];
+        }
+        public void LoadDataFromCSV(string location)
+        {
+            List<string> loadedQuestions = new List<string>();
+            List<string> loadedAnswers = new List<string>();
+
+            using (var reader = new StreamReader(location))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    if (values.Length >= 2)
+                    {
+                        loadedQuestions.Add(values[0]);
+                        loadedAnswers.Add(values[1]);
+                    }
+                }
+            }
+
+            questions = loadedQuestions.ToArray();
+            answers = loadedAnswers.ToArray();
+        }
+        public string generate(string c)
+        {
+            double maxSimilarity = 0;
+            int mostSimilarIndex = -1;
+
+            for (int i = 0; i < questions.Length; i++)
+            {
+                double similarity = StringOperations.LevenshteinSimilarity(c, questions[i]);
+                if (similarity > maxSimilarity)
+                {
+                    maxSimilarity = similarity;
+                    mostSimilarIndex = i;
+                }
+            }
+
+            if (mostSimilarIndex != -1)
+            {
+                return answers[mostSimilarIndex];
+            }
+
+            // If no similar question was found, return a default response
+            return "I'm sorry, I don't have an answer to that question.";
+        }
+    }
+}
---
+++ learning machine learning/Simple/Chatbot stuff/StringOperations.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learning_machine_learning.Simple
+{
+    public class StringOperations
+    {
+        #region Levenshtein
+        public static int LevenshteinDistance(string source, string target)
+        {
+            int sourceLength = source.Length;
+            int targetLength = target.Length;
+
+            int[,] distanceMatrix = new int[sourceLength + 1, targetLength + 1];
+
+            // Initialize the first row and column of the matrix
+            for (int i = 0; i <= sourceLength; i++)
+            {
+                distanceMatrix[i, 0] = i;
+            }
+
+            for (int j = 0; j <= targetLength; j++)
+            {
+                distanceMatrix[0, j] = j;
+            }
+
+            // Calculate the minimum distance
+            for (int i = 1; i <= sourceLength; i++)
+            {
+                for (int j = 1; j <= targetLength; j++)
+                {
+                    int cost = (source[i - 1] == target[j - 1]) ? 0 : 1;
+
+                    distanceMatrix[i, j] = Math.Min(
+                        Math.Min(distanceMatrix[i - 1, j] + 1, distanceMatrix[i, j - 1] + 1),
+                        distanceMatrix[i - 1, j - 1] + cost);
+                }
+            }
+
+            // Return the Levenshtein distance
+            return distanceMatrix[sourceLength, targetLength];
+        }
+        public static double LevenshteinSimilarity(string source, string target)
+        {
+            int distance = LevenshteinDistance(source, target);
+            int maxLength = Math.Max(source.Length, target.Length);
+
+            return 1 - (double)distance / maxLength;
+        }
+        #endregion
+    }
+}
